23c9cef9d1c0373a30d0ab03fef8b3080bcbd7fd
Jeerawat Keawsawat <gukaihero@gmail.com>
2019-09-18 12:02:07 +0700

    initial server

--- src/index.js
+++ src/index.js
@@ -1,2 +1,39 @@
-// [1] importing the dependencies
+const MODULE_ID = "app:main";
 
+// [1] importing the dependencies
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+// [2] defining the express app
+const app = express();
+
+/**
+ * [3] adding middleware
+ * adding Helmet to enhance your API's security
+ * using bodyParser to parse JSON bodies into JS objects
+ * enabling CORS for all requests
+ * enabling morgan to log HTTP requests
+ */
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan("combined"));
+
+/**
+ * Testing call api
+ */
+app.get("/test", (req, res) => {
+  res.json({
+    status: "Hello World API's"
+  });
+});
+
+// [4] Starting server
+app.listen(3001, () => {
+  console.log("listening on port 3001");
+});
+
+module.exports = app;
